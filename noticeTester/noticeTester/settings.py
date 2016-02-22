a51bd5c950298871bf208f683ae729a814ab1b9b
Mattias Edin <mattias.edin92@gmail.com>
2016-02-22 14:32:20 +0100

    settings changes

--- noticeTester/noticeTester/settings.py
+++ noticeTester/noticeTester/settings.py
@@ -120,6 +120,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 WSGI_APPLICATION = 'noticeTester.wsgi.application'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
