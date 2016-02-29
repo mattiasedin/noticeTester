709822724c1054cd1ee35af559016efeb5285f11
Mattias Edin <mattias.edin92@gmail.com>
2016-02-29 13:47:22 +0100

    changes

--- pusher/views.py
+++ pusher/views.py
@@ -7,9 +7,11 @@ from django.contrib.auth.models import User, Group
 
 from rest_framework import viewsets, generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 import jwt
 
@@ -53,12 +55,18 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-
-@csrf_exempt
+@api_view(['GET'])
+@authentication_classes((SessionAuthentication, BasicAuthentication))
+@permission_classes((IsAuthenticated,))
 def user_list(request):
     """
     List all code snippets, or create a new snippet.
     """
+    content = {
+        'user': unicode(request.user),  # `django.contrib.auth.User` instance.
+        'auth': unicode(request.auth),  # None
+    }
+
     if request.method == 'GET':
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
