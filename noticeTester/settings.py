9b14017ddc5caf40309ad4fc9b62b1a86e25e242
Mattias Edin <mattias.edin92@gmail.com>
2016-03-13 11:15:23 +0100

    new requirement

--- noticeTester/settings.py
+++ noticeTester/settings.py
@@ -59,24 +59,24 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'noticeTester.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
 
 
-#TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates'), os.path.join(PROJECT_ROOT, 'templates')]
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates'), os.path.join(PROJECT_ROOT, 'templates')]
 
 WSGI_APPLICATION = 'noticeTester.wsgi.application'
 
