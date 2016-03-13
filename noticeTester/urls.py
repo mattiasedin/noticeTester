d3d71def00446c0e2ffb0d973b5366b2a2d3ec7f
Mattias Edin <mattias.edin92@gmail.com>
2016-03-13 11:40:12 +0100

    fixed redirect login

--- noticeTester/urls.py
+++ noticeTester/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     url(r'^', include('pusher.urls', namespace="pusher")),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, name="login"),
+    url(r'^login', auth_views.login, name="login"),
     url(r'^logout/$', auth_views.logout, name="logout"),
 ]
 # url(r'^pusher/', include('pusher.urls', namespace="pusher")),
